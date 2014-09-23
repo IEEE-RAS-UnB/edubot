5b74e8f83a569f1425176b6018a9283d9286d78a
mendelson <m.mendelson.unb@gmail.com>
2014-09-22 23:07:17 -0300

    Add Class 3 and new codes

--- Aula 03/Códigos/aula_03_cod_1/aula_03_cod_1.ino
+++ Aula 03/Códigos/aula_03_cod_1/aula_03_cod_1.ino
@@ -9,7 +9,7 @@
 #include <Sparki.h>
 
 //Se houver um obstáculo a uma distância menor do que essa, o robô irá tentar desviar
-const int distancia_minima 20
+const int distancia_minima = 20
 
 //Funções
 void imprimir (int); //Imprime distância na frente
@@ -30,12 +30,12 @@ void loop()
 {
     sparki.moveForward (); //O robô começa a andar para frente
     
-    distancia_frente = sparki.ping(); //Mede a distancia, usando o Sonar
+    distancia_frente = sparki.ping(); //Mede a distância, usando o Sonar
     
     //Vamos imprimir no monitor LCD do Sparki os valores que estão sendo lidos:
     imprimir (distancia_frente);
     
-    if(distancia_frente < distancia_minima && distancia_frente != -1) //A medição "-1" occore quando há algum erro. Neste caso, devemos simplesmente ignorar e fazer uma nova leitura
+    if (distancia_frente <= distancia_minima && distancia_frente != -1) //A medição "-1" occore quando há algum erro. Neste caso, devemos simplesmente ignorar e fazer uma nova leitura
     {
       sparki.moveStop (); //O robô para, pois há um obstáculo a frente e ele precisa desviar!
       imprimir ("Estou muito perto de um obstáculo! Preciso desviar!");
@@ -43,16 +43,14 @@ void loop()
       //Agora, o robô deve decidir para onde ir. Lembre-se, ângulos positivos fazem o robô olhar para a direita; negativos, para a esquerda!
       sparki.servo (70); //Olha para a direita
       delay (1000); //Esperamos um pouco para que o Sonar esteja na posição desejada
-
+      
       distancia_direita = sparki.ping (); //Faz a medição
       
       sparki.servo (-70); //Olha para a esquerda
       delay (1000); //Esperamos um pouco para que o Sonar esteja na posição desejada
       
-      distancia_esquerda = sparki.ping (); //Faz a medição
-      
-      sparki.servo (0); //Olha para a frente
-      
+      sparki.servo (0); //Volta a olhar para frente
+    
       //Vamos imprimir no monitor LCD do Sparki os valores que estão sendo lidos:
       imprimir (distancia_direita, distancia_esquerda);
       
@@ -100,3 +98,4 @@ void imprimir (char *mensagem)
   sparki.print (mensagem);
   sparki.updateLCD (); //Imprime na tela
 }
+
