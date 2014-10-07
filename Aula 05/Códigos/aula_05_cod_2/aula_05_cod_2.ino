ead690f13822f34c3ee4f85720df6ff093298b0a
mendelson <m.mendelson.unb@gmail.com>
2014-10-07 18:46:01 -0300

    Class 05 and instructions for SparkiDuino updated

--- Aula 05/Códigos/aula_05_cod_2/aula_05_cod_2.ino
+++ Aula 05/Códigos/aula_05_cod_2/aula_05_cod_2.ino
@@ -5,7 +5,7 @@ int sensorEsquerda;
 int sensorCentral;
 int sensorDireita;
 
-int limite = 500;
+int limite = 700;
 
 // Função para imprimir os valores na tela
 void imprimir();
@@ -17,41 +17,43 @@ void setup()
 
 void loop()
 {
-  sensorEsquerda = sparki.lineLeft();
   sensorCentral = sparki.lineCenter();
-  sensorDireita = sparki.lineRight();
-  
-  if ( sensorEsquerda < limite )
-  {  
-    sparki.moveLeft();
-  }
 
-  if ( sensorDireita < limite )
-  {  
-    sparki.moveRight();
-  }
-
-  if ( (sensorCentral < limite) && (sensorEsquerda > limite) && (sensorDireita > limite) )
+  while(sensorCentral <= limite)
   {
     sparki.moveForward();
+    sensorCentral = sparki.lineCenter();
+    imprimir();
+  }
+  
+  sensorEsquerda = sparki.lineLeft();
+  sensorDireita = sparki.lineRight();
+  
+  if(sensorEsquerda < sensorDireita)
+  {
+    sparki.moveLeft();
+  }
+  else
+  {
+    sparki.moveRight();
   }
   
   imprimir();
   
-  delay(100); // wait 0.1 seconds
+  //delay(100); // wait 0.1 seconds
 }
 
 void imprimir ()
 {
   sparki.clearLCD ();
 
-  sparki.print("Sensor na esquerda: ");
+  sparki.print("Esquerda: ");
   sparki.println(sensorEsquerda);
   
-  sparki.print("Sensor central: ");
+  sparki.print("Centro:   ");
   sparki.println(sensorCentral);
   
-  sparki.print("Sensor na direita: ");
+  sparki.print("Direita:  ");
   sparki.println(sensorDireita);
   
   sparki.updateLCD();
